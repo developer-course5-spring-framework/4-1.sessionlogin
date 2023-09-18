83a22073a530d0d9f0c7e120515790710090f840
kokojihye <jihyevivianako@gmail.com>
2023-09-19 03:12:30 +0900

    ## 404에러 MainController, main.html 추가

---
+++ src/main/java/com/ohgiraffers/sessionlogin/config/Chap01SessionLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.sessionlogin.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01SessionLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01SessionLoginApplication.class, args);
+    }
+
+}
